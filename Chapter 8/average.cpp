59163f2d2701eb2700f3f59a853ae3b4c7c4ebb1
blackroseangel <blackroseangel@gmail.com>
2016-02-28 19:57:40 -0500

    Finished exercise 8-3(average)

---
+++ Chapter 8/average.cpp
@@ -0,0 +1,49 @@
+/**************************************************************************
+
+* Program: Average
+
+* Created: 25.2.16 ; Modified: 25.2.16
+
+* Version: 1.3
+
+* Creator: Brett Chapin(bachapin)
+
+* Purpose: This is exercise 8-3 from Practical C++
+  Write a program to average n numbers.
+
+**************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int input, runTotal, output, count;
+  bool stop = false;
+  
+  count = 0;
+  runTotal = 0;
+  output = 0;
+  
+  cout << "This program will average a range of numbers." << endl;
+  cout << "Please enter 0 to quit." << endl;
+  
+  do {
+    cout << "Number? ";
+    cin >> input;
+    
+    if (input != 0)
+      runTotal += input;
+    else
+      stop = true;
+    
+    count++;
+    
+  } while(stop == false);
+  
+  output = runTotal / count;
+  
+  cout << "You entered " << count << " numbers, that average to " << output << endl;
+
+  return(0);
+} //end main()
