b5f9bd9121d87e7cd2674261349d51d3756bfd26
BlackRoseAngel <BlackRoseAngel@gmail.com>
2016-03-02 19:51:05 -0500

    Made strFinder in scrap folder

---
+++ scrap/scrap/2.mar.2016/strFinder.cpp
@@ -0,0 +1,26 @@
+/***************************************************************************
+
+* Program: String Finder
+
+* Created: 2.3.16 ; Modified: 2.3.16
+
+* Version: 1.0
+
+* Creator: Brett Chapin(bachapin)
+
+* Purpose: This program is designed to come up with a function for searching
+  for a particular string within a string using a method to which is more
+  accurate than just doing a string comparison.
+
+***************************************************************************/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  
+
+  return(0);
+} //end main()
