964a7a9c0fe23086c80fbf189afae93fbab09a4f
BlackRoseAngel <BlackRoseAngel@gmail.com>
2017-01-15 20:25:32 -0500

    Finished vowel program, and added its output file.

--- Chapter 8/vowel.cpp
+++ Chapter 8/vowel.cpp
@@ -1,26 +1,49 @@
 /**************************************************************************
 
-* Program:
+* Program: Vowel
 
-* Created: ; Modified: 
+* Created: 1.15.17 ; Modified: 1.15.17
 
-* Version:
+* Version: 0.1
 
 * Creator: Brett Chapin(bachapin)
 
-* Purpose:
+* Purpose: This is exercise 8-5 from Practical C++
+  Write a program that reads a character and prints out whether it is a
+  vowel or consonant.
 
 **************************************************************************/
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <fstream>
-#include <cmath>
 
 using namespace std;
 
+bool vowel(char test);
+
 int main()
 {
+	char input;
+
+	cout << "Please enter a character: ";
+	cin >> input;
+
+	if (vowel(input) == true)
+		cout << input << " is a vowel." << endl;
+	else
+		cout << input << " is a consonant." << endl;
 
   return(0);
 } //end main()
+
+bool vowel(char test){
+	char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+	bool output = false;
+
+	for(int x = 0; x < 5; x++){
+		if (test == vowels[x])
+			output = true;
+	} //end for-loop
+
+	return output;
+} //end vowel()
